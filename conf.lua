7d4295b427a8090c53163f166f881251e894697c
Jacob Poteet <64765148+JacobPoteet@users.noreply.github.com>
2025-04-19 22:43:03 -0400

    Add config file and set resolution to 1080p 4:3

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.width = 1440 -- 4:3 aspect ratio width for 1080p
+    t.window.height = 1080 -- 4:3 aspect ratio height for 1080p
+    t.window.resizable = false -- Disable resizing to maintain aspect ratio
+    t.window.title = "Dialogue Jam" -- Set the game window title
+end
